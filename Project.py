f7a661f26dceb556bd133d0182511b06bbfa3cdb
kelper12 <kelperharry12@gmail.com>
2026-05-07 09:11:35 +0000

    Implement password strength evaluation logic

--- Project.py
+++ Project.py
@@ -1,1 +1,17 @@
+if len(password) >= 8:
+    strength += 1
+if re.search(r"[A-Z]", password):
+    strength += 1
+if re.search(r"[a-z]", password):
+    strength += 1
+if re.search(r"[0-9]", password):
+    strength += 1
+if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+    strength += 1
 
+if strength <= 2:
+    return "Weak"
+elif strength == 3 or strength == 4:
+    return "Medium"
+else:
+    return "Strong"
